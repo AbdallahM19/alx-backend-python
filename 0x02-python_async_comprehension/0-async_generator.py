f1badd2560a20612a5b54d187e71f02545d5943a
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-07-09 04:46:34 -0700

    async comprehension

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""pycode"""
+
+import asyncio
+import random
+# from typing import Generator
+
+
+async def async_generator():
+    """async_generator function"""
+    for i in range(10):
+        yield random.uniform(0, 10)
+        await asyncio.sleep(1)
