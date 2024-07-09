996a397ec0aeea01783bb1b088105ae841c4297d
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-07-09 05:23:02 -0700

    async comprehension

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""pycode"""
+
+import asyncio
+from time import time
+from typing import List
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> List[float]:
+    """measure_runtime function"""
+    total_time = time()
+    tasks = [async_comprehension() for i in range(4)]
+    await asyncio.gather(*tasks)
+    total_time = time() - total_time
+    return total_time
