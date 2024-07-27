0299d48587d28e4e8dc638313f0cabb05af0c000
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-07-27 00:04:52 -0700

    Task 3 (DONE) ✔

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,7 +4,7 @@
 import unittest
 from unittest.mock import patch, Mock
 from parameterized import parameterized
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 from typing import (
     Mapping,
     Sequence,
@@ -69,3 +69,26 @@ class TestGetJson(unittest.TestCase):
             mock_get.return_value.json.return_value = test_pay
             self.assertEqual(get_json(url), test_pay)
             mock_get.assert_called_once_with(url)
+
+
+class TestMemoize(unittest.TestCase):
+    """TestMemoize class that inherits from unittest.TestCase"""
+    def test_memoize(self) -> None:
+        """test_memoize function"""
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(
+            TestClass,
+            "a_method",
+            return_value=42,
+        ) as test_memoized:
+            test_obj = TestClass()
+            self.assertEqual(test_obj.a_property, 42)
+            self.assertEqual(test_obj.a_property, 42)
+            test_memoized.assert_called_once()
