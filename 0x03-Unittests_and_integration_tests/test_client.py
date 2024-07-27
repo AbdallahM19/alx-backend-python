eafd751c725123e4e1d867776950297d549f9e0e
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-07-27 02:10:07 -0700

    Task 4 (DONE) ✔

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -11,22 +11,20 @@ from typing import Dict
 class TestGithubOrgClient(unittest.TestCase):
     """TestGithubOrgClient class"""
     @parameterized.expand([
-        ("google"),
-        ("abc"),
+        ("google", {'repo_name': "google"}),
+        ("abc", {'repo_name': "abc"}),
     ])
     @patch("client.get_json")
     def test_org(
         self,
         org: str,
+        params: Dict,
         mock_get_json: MagicMock,
     ) -> None:
         """test_org"""
-        expected_json = {
-            "repos_url": "https://api.github.com/orgs/{}".format(org)
-        }
-        mock_get_json.return_value = expected_json
+        mock_get_json.return_value = MagicMock(return_value=params)
         class_test = GithubOrgClient(org)
-        self.assertEqual(class_test.org, expected_json)
+        self.assertEqual(class_test.org(), params)
         mock_get_json.assert_called_once_with(
             "https://api.github.com/orgs/{}".format(org)
         )
