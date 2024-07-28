7ded9f2aa30673d54288c624d9c1eca55fc9d485
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-07-28 05:15:22 -0700

    Task 7 (DONE) ✔

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -105,8 +105,8 @@ class TestGithubOrgClient(unittest.TestCase):
         mock_test.assert_called_once()
 
     @parameterized.expand([
-        ({"license": {"key": "bsd-3-clause"}}, "bsd-3-clause", True),
-        ({"license": {"key": "bsl-1.0"}}, "bsd-3-clause", False),
+        ({'license': {'key': "bsd-3-clause"}}, "bsd-3-clause", True),
+        ({'license': {'key': "bsl-1.0"}}, "bsd-3-clause", False),
     ])
     def test_has_license(
         self, licence_dict: Dict, name: str, expected: bool
